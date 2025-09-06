2126caac32776cc6a8793496f01b2cfeaa839f2a
hemangzellis <hemang@zellis.com.au>
2025-09-06 15:54:53 +0530

    login url bug fixed

---
+++ src/app/login/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/context/auth-context";
+import { useGlobalLoader } from "@/context/loader-context";
+import Cookies from "js-cookie";
+
+export default function Home() {
+  const router = useRouter();
+  const { user, loading } = useAuth();
+  const { showLoader } = useGlobalLoader();
+
+  useEffect(() => {
+    if (loading) {
+      showLoader();
+      return;
+    }
+    const token = Cookies.get("id_token");
+    if (user && token) {
+      const tenant = Cookies.get("tenant");
+      if (tenant) {
+        router.replace("/dashboard");
+      } else {
+        router.replace("/tenants");
+      }
+      return;
+    }
+    // 🔴 If not authenticated, redirect to Cognito login
+    // console.log(process.env.NEXT_PUBLIC_API_BASE_URL);
+    window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/login`;
+  }, [user, loading, router, showLoader]); // <-- added router and showLoader
+
+  return null;
+}
